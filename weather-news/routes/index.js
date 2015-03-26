e3586abf7b194ac9808257a780ae0ff533b25b72
EC2 Default User <ec2-user@ip-172-31-27-4.ec2.internal>
2015-03-26 16:41:06 +0000

    Finished the lab

--- weather-news/routes/index.js
+++ weather-news/routes/index.js
@@ -7,9 +7,10 @@ var Comment = mongoose.model('Comment');
 
 router.param('post', function(req, res, next, id) {
 	var query = Post.findById(id);
+	console.log("Inside the post param");
 	query.exec(function(err, post) {
 		if (err) { return next(err); }
-		if (!post) { return next(new Error("can't find post"); }
+		if (!post) { return next(new Error("can't find post")); }
 		req.post = post;
 		return next();
 	});
@@ -23,6 +24,21 @@ router.param('comment', function(req, res, next, id){
 		req.comment = comment;
 		return next();
 	});
+});
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+    res.json(posts);
+  });
+});
+
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+  post.save(function(err, post){
+  if(err){ return next(err); }
+    res.json(post);
+  });
 });
 
 router.get('/posts/:post', function(req, res, next){
@@ -58,21 +74,6 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
 		res.json(comment);
 	});
 });
-
-/*router.get('/posts', function(req, res, next) {
-	Post.find(function(err, posts) {
-		if (error) { return next(err); }
-		res.json(posts);
-	})
-});*/
-
-/*router.post('/posts', function(req, res, next){
-	var post = new Post(req.body);
-	post.save(function(err, post) {
-		if (err) { return next(err); }
-		res.json(post);
-	});
-});*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
